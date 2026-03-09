9dfef85fd4091a1b2477d5d4d71b791d31dde909
ShibaNoi <evergreen19832@gmail.com>
2026-03-09 23:32:49 +0700

    lab24_1.cpp completed

--- lab24_1.cpp
+++ lab24_1.cpp
@@ -64,3 +64,23 @@ void List::append(int d){
 }
 
 //Write List::remove() here
+void List::remove(int idx){
+    Node *toDelete;
+    
+    if(idx == 0){
+        toDelete = root;
+        root = root->next;
+        delete toDelete;
+        size--;
+        return;
+    }
+    
+    Node *current = root;
+    for(int i = 0; i < idx-1; i++){
+        current = current->next;
+    }
+    toDelete = current->next;
+    current->next = toDelete->next;
+    delete toDelete;
+    size--;
+}
